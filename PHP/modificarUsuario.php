7b238338b62c1abc51dd199614d065d64bea69bd
Kevin Carracedo Vázquez <kcv239@inlumine.ual.es>
2018-06-26 13:01:55 +0200

    Checking...

--- PHP/modificarUsuario.php
+++ PHP/modificarUsuario.php
@@ -8,13 +8,13 @@ $contra = $_POST['contra'];
 $activo = $_POST['activo'];
 $userID = $_POST['ID'];
 
-$modUser = mysqli_query($con, "UPDATE usuarios SET email = '".$email."', nombre = '', contra = '', activo = '' WHERE userID = '".$userID."'");
+$modUser = mysqli_query($connection, "UPDATE usuarios SET email = '".$email."', nombre = '', contra = '', activo = '' WHERE userID = '".$userID."'");
 
 if(!$modUser){
     echo "No se ha podido modificar el usuario." . mysql_error();
-    $con->close();
+    $connection->close();
     exit;
 }
 
-$con->close();
+$connection->close();
 ?>
